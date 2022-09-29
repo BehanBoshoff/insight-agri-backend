0b02dd19e808573f3caa7c52e5de9a324e8c0c75
Behan Boshoff <behan.boshoff@jumo.world>
2022-09-29 19:35:47 +0200

    Create User Complete

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from db.config import settings
-# from api.v1.general_pages.route_homepage import general_pages_router
+from api.base import api_router
 from db.session import engine
 from db.base import Base
 
 
 def include_router(app):
-    # app.include_router(general_pages_router)
-    print("...")
+    app.include_router(api_router)
 
 
 def configure_static(app):
