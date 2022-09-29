645c23b266ca7d9023c51ad813c5d248eaa557bb
Behan Boshoff <behan.boshoff@jumo.world>
2022-09-29 20:36:55 +0200

    Add Jobs Routes & Test

--- api/base.py
+++ api/base.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .v1 import route_users
+from .v1 import route_jobs
 
 api_router = APIRouter()
 api_router.include_router(route_users.router,prefix="/users",tags=["users"])
+api_router.include_router(route_jobs.router,prefix="/jobs",tags=["jobs"])
