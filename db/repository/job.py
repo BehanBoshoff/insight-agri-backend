8d7b269aa17f324c77b0b1918ff9564f874ac461
Behan Boshoff <behan.boshoff@jumo.world>
2022-09-29 20:41:26 +0200

    Add Get Job Route & Test

--- db/repository/job.py
+++ db/repository/job.py
@@ -12,3 +12,8 @@ def create_new_job(job: JobCreate, db: Session, owner_id: int):
     db.refresh(job_object)
 
     return job_object
+
+
+def retreive_job(id: int, db: Session):
+    item = db.query(Job).filter(Job.id == id).first()
+    return item
