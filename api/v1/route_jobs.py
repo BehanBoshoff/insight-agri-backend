12ce8d14e6fe9ead7a6907ee3c511cdd4a541425
Behan Boshoff <behan.boshoff@jumo.world>
2022-09-29 22:01:19 +0200

    Add Delete Job With User Authorization

--- api/v1/route_jobs.py
+++ api/v1/route_jobs.py
@@ -6,8 +6,10 @@ from fastapi import HTTPException
 from fastapi import status
 from sqlalchemy.orm import Session
 
+from api.v1.route_login import get_current_user_from_token
 from core.schemas.job import JobCreate
 from core.schemas.job import ShowJob
+from db.models.user import User
 from db.repository.job import create_new_job
 from db.repository.job import delete_job_by_id
 from db.repository.job import list_jobs
@@ -59,11 +61,21 @@ def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
 
 
 @router.delete("/delete/{id}")
-def delete_job(id: int, db: Session = Depends(get_db)):
-    current_user_id = 1
-    message = delete_job_by_id(id=id, db=db, owner_id=current_user_id)
-    if not message:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id {id} not found"
+def delete_job(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+):
+    job = retreive_job(id=id, db=db)
+    if not job:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Job with {id} does not exist",
         )
-    return {"msg": "Successfully deleted."}
+    print(job.owner_id, current_user.id, current_user.is_superuser)
+    if job.owner_id == current_user.id or current_user.is_superuser:
+        delete_job_by_id(id=id, db=db, owner_id=current_user.id)
+        return {"msg": "Successfully deleted."}
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not permitted!"
+    )
