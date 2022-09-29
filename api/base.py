a2588dd86d05b398cecedc0996471ed8d70878cf
Behan Boshoff <behan.boshoff@jumo.world>
2022-09-29 21:49:48 +0200

    Add Login Route

--- api/base.py
+++ api/base.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .v1 import route_jobs
+from .v1 import route_login
 from .v1 import route_users
 
 api_router = APIRouter()
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
 api_router.include_router(route_jobs.router, prefix="/jobs", tags=["jobs"])
+api_router.include_router(route_login.router, prefix="/login", tags=["login"])
